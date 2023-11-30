bae682e2f8c3416c10c49ba298d3879940c5a279
Paul <whatevergeek@outlook.com>
2023-11-30 10:37:34 +0800

    Lab 2: Step 4: Solace Session Setup

--- TopicSubscriber/TopicSubscriber.cs
+++ TopicSubscriber/TopicSubscriber.cs
@@ -4,6 +4,10 @@ namespace TopicSubscriber
 {
     internal class TopicSubscriber
     {
+        string VPNName { get; set; }
+        string UserName { get; set; }
+        string Password { get; set; }
+
         static void Main(string[] args)
         {
             if (args.Length < 3)
@@ -28,10 +32,21 @@ namespace TopicSubscriber
             ContextFactory.Instance.Init(cfp);
             try
             {
+                // Context must be created first
                 using (IContext context = ContextFactory.Instance.CreateContext(new ContextProperties(), null))
                 {
                     Console.WriteLine("Context Created");
-
+                    // Create the application
+                    TopicSubscriber topicSubscriber = new TopicSubscriber()
+                    {
+                        VPNName = vpnname,
+                        UserName = username,
+                        Password = password
+                    };
+                    {
+                        // Run the application within the context and against the host
+                        topicSubscriber.Run(context, host);
+                    }
                 }
             }
             catch (Exception ex)
@@ -45,6 +60,35 @@ namespace TopicSubscriber
             }
             Console.WriteLine("Finished.");
         }
+
+        void Run(IContext context, string host)
+        {
+            // Create session properties
+            SessionProperties sessionProps = new SessionProperties()
+            {
+                Host = host,
+                VPNName = VPNName,
+                UserName = UserName,
+                Password = Password
+            };
+
+            // Connect to the Solace messaging router
+            Console.WriteLine("Connecting as {0}@{1} on {2}...", UserName, VPNName, host);
+            // NOTICE HandleMessage as the message event handler
+            using (ISession session = context.CreateSession(sessionProps, null, null))
+            {
+                ReturnCode returnCode = session.Connect();
+                if (returnCode == ReturnCode.SOLCLIENT_OK)
+                {
+                    Console.WriteLine("Session successfully connected.");
+                }
+                else
+                {
+                    Console.WriteLine("Error connecting, return code: {0}", returnCode);
+                }
+            }
+        }
+
     }
 
 }
